b3751ca7f3b85d859b662fe5e155c0c525ac6ec2
“Denty” <dentytxr@gmail.com>
2026-06-05 13:22:20 -0400

    .

---
+++ DisableFinalTargetDot/EntryPpppppppp.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using LabApi;
+using LabApi.Loader.Features.Plugins;
+using System.Reflection;
+
+namespace DisableFinalTargetDot
+{
+    public class EntryPpppppppp : Plugin
+    {
+        public override string Name => "DisableFinalTargetDot";
+        public override string Description => "Disables red dot for final human player";
+        public override string Author => "Denty";
+        public override Version RequiredApiVersion => new Version("1.0.0");
+
+        public static Harmony Harmony;
+
+        public override void Disable()
+        {
+            Harmony.UnpatchAll(Harmony.Id);
+        }
+
+        public override void Enable()
+        {
+            HarmonyLib.Harmony.DEBUG = true;
+            Harmony = new Harmony($"{Author}.{Name}");
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ DisableFinalTargetDot/Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using PlayerRoles;
+using PlayerRoles.PlayableScps.HumanTracker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mirror;
+
+namespace DisableFinalTargetDot
+{
+    [HarmonyPatch(typeof(LastHumanTracker), "ServerUpdateLastHumanTracking")]
+    public class BlockTrackingPatch()
+    {
+        private static bool Prefix(LastHumanTracker __instance)
+        {
+            __instance.Network_lastTargetNetId = 0U;
+            __instance.Network_lastTargetPos = null;
+            return false;
+        }
+    }
+}
